67e997782529ceb63a90086c58dcbed8ac8844be
spark156 <lucabergman@hotmail.nl>
2021-12-12 23:31:31 +0100

    add linting and fix linting errors

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,13 +1,14 @@
+"""api for spotify auto monthly playlist"""
 from typing import Optional
+import base64
+from random import choice
+from string import ascii_uppercase
+from urllib.parse import urlencode, quote_plus
 
 from fastapi import FastAPI, Response, Cookie
 from fastapi.responses import RedirectResponse
 
-from random import choice
-from string import ascii_uppercase
-from urllib.parse import urlencode, quote_plus
 import requests
-import base64
 
 from . import config
 
@@ -16,8 +17,9 @@ app = FastAPI(root_path="/api/v1")
 
 @app.get("/login", response_class=RedirectResponse)
 def login(response: Response):
+    """redirect user to login screen from spotify and set state to persist in browser cookie"""
     state = ''.join(choice(ascii_uppercase) for i in range(16))
-    response.set_cookie(key='state_cookie', value=state)
+    response.set_cookie(key='state_cookie', value=state, httponly=True, secure=True)
     return 'https://accounts.spotify.com/authorize?' + urlencode({
         'response_type': 'code',
         'client_id': config.client_id,
@@ -27,16 +29,23 @@ def login(response: Response):
     }, quote_via=quote_plus)
 
 @app.get("/callback", response_class=RedirectResponse)
-def auth_callback(response: Response, state: str, state_cookie: Optional[str] = Cookie(None), code: Optional[str] = None, error: Optional[str] = None):
+def auth_callback(
+        response: Response,
+        state: str,
+        state_cookie: Optional[str] = Cookie(None),
+        code: Optional[str] = None,
+        error: Optional[str] = None
+    ):
+    """check state for tampering and get access and refresh tokens if all is okay"""
     if state != state_cookie:
         # request has been tampered with
         return "States do not match up, your request may have been tempered with."
     else:
         response.delete_cookie('state_cookie')
-    
+
     if error:
         return error
-    
+
     if code:
         message = config.client_id + ':' + config.client_secret
         r = requests.post('https://accounts.spotify.com/api/token', data={
