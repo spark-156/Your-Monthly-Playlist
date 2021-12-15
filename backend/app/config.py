f23316de0e840f03efb4d69e51f35a36f1121803
spark156 <lucabergman@hotmail.nl>
2021-12-15 14:46:22 +0100

    update config.pu

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,6 +1,7 @@
+"""All environment variables"""
 import os
 
 client_id = os.environ['CLIENT_ID']
 client_secret = os.environ['CLIENT_SECRET']
 redirect_uri = os.environ['REDIRECT_URI']
-scope = 'user-read-private user-read-email'
+access_scope = 'user-read-private user-read-email'
