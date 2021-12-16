f8c9bfef83ca92beac3ad91eaede2e09961fe03a
spark156 <lucabergman@hotmail.nl>
2021-12-16 14:48:06 +0100

    update styling

--- frontend/src/lib/constants.ts
+++ frontend/src/lib/constants.ts
@@ -1,8 +1,9 @@
 enum Colors {
-  Black = "#141515",
+  Black = "#191414",
   White = "#ffffff",
   Yellow = "#FAFF81",
-  Blue = "#75DDDD"
+  Blue = "#75DDDD",
+  Green = "#1DB954"
 }
 
 enum Fonts {
