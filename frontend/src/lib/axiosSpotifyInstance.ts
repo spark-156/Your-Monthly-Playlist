fa2415e0d16583be61c666c1a9944780b1892dee
spark156 <lucabergman@hotmail.nl>
2021-12-17 15:13:46 +0100

    linting

--- frontend/src/lib/axiosSpotifyInstance.ts
+++ frontend/src/lib/axiosSpotifyInstance.ts
@@ -1,29 +1,29 @@
-import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
-import { getCookie } from "./getCookie";
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios'
+import { getCookie } from './getCookie'
 
 const axiosSpotifyInstance = axios.create({
   baseURL: 'https://api.spotify.com/v1',
   timeout: 10000,
   headers: {
-    "Authorization": `Bearer ${getCookie("access_token")}`
+    Authorization: `Bearer ${getCookie('access_token')}`
   }
-});
+})
 
 axiosSpotifyInstance.interceptors.request.use(async (config: AxiosRequestConfig) => {
-  if (!getCookie("access_token")) { // access token expired
+  if (!getCookie('access_token')) { // access token expired
     window.location.href = '/api/v1/refresh'
-  } 
+  }
   return config
 })
 
 axiosSpotifyInstance.interceptors.response.use((response: AxiosResponse) => {
   if (response.status === 401) {
-    window.location.href = "/api/v1/login"
+    window.location.href = '/api/v1/login'
   } else if (response.status === 307) {
-      console.log("redirecting")
+    console.log('redirecting')
   }
 
   return response
 })
 
-export { axiosSpotifyInstance };
+export { axiosSpotifyInstance }
