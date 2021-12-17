2cf06d4721a390e5fbf62c50987dc1f65572a23e
spark156 <lucabergman@hotmail.nl>
2021-12-17 16:38:38 +0100

    added extra access scope

--- backend/app/config.py
+++ backend/app/config.py
@@ -4,4 +4,4 @@ import os
 client_id = os.environ['CLIENT_ID']
 client_secret = os.environ['CLIENT_SECRET']
 redirect_uri = os.environ['REDIRECT_URI']
-access_scope = 'user-read-private user-read-email'
+access_scope = 'user-read-private user-read-email user-library-read'
