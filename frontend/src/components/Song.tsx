37ae24d9ec0554b6d25df9ff720dbe2d7083f5e3
spark156 <lucabergman@hotmail.nl>
2021-12-21 22:02:58 +0100

    better song artist rendering

--- frontend/src/components/Song.tsx
+++ frontend/src/components/Song.tsx
@@ -15,8 +15,6 @@ export function Song ({ imageSrc, songTitle, artists, className, ...props }: Son
   return <div className={classNames(styles.container, className)} {...props}>
     <img src={imageSrc} alt='' loading='lazy' className={styles.image} />
     <TextDiv className={styles.title}>{songTitle}</TextDiv>
-    <div className={styles.artists}>
-      {artists.map(artist => <TextDiv className={styles.artist} key={artist.name}>{artist.name}</TextDiv>)}
-    </div>
+    <TextDiv className={styles.artist}>{artists.map(artist => artist.name).join(', ')}</TextDiv>
   </div>
 }
