548fbf24592897d53d75ee8a2239478b9172f656
spark156 <lucabergman@hotmail.nl>
2021-12-22 11:51:48 +0100

    update Types

---
+++ frontend/src/types/artistsType.ts
@@ -0,0 +1,31 @@
+export interface GetArtists {
+  artists: ArtistExpansive[];
+}
+
+export interface ArtistExpansive {
+  external_urls: ExternalUrls;
+  followers: Followers;
+  genres: string[];
+  href: string;
+  id: string;
+  images: Image[];
+  name: string;
+  popularity: number;
+  type: string;
+  uri: string;
+}
+
+export interface ExternalUrls {
+  spotify: string;
+}
+
+export interface Followers {
+  href: string;
+  total: number;
+}
+
+export interface Image {
+  url: string;
+  height: number;
+  width: number;
+}
