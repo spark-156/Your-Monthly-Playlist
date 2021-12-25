a5005fa5c8a336bc2966ff8e603a302f8d3eb269
spark156 <lucabergman@hotmail.nl>
2021-12-25 22:07:23 +0100

    fix ts errors

--- frontend/src/components/Arrow.tsx
+++ frontend/src/components/Arrow.tsx
@@ -12,7 +12,7 @@ export enum DirectionEnum {
   /* eslint-enable no-unused-vars */
 }
 
-interface ArrowProps extends React.HTMLAttributes<HTMLDivElement> {
+interface ArrowProps extends React.HTMLAttributes<SVGSVGElement> {
   direction: DirectionEnum
 }
 
@@ -20,15 +20,14 @@ export function Arrow ({ direction, className, onClick, ...props }: ArrowProps) 
   // TODO FIX left and right versions of arrow
 
   return <div
-  className={classNames(styles.arrow, {
-    [styles.up]: direction === DirectionEnum.Up,
-    [styles.down]: direction === DirectionEnum.Down,
-    [styles.left]: direction === DirectionEnum.Left,
-    [styles.right]: direction === DirectionEnum.Right
-  }, className)}
-  {...props}
+    className={classNames(styles.arrow, {
+      [styles.up]: direction === DirectionEnum.Up,
+      [styles.down]: direction === DirectionEnum.Down,
+      [styles.left]: direction === DirectionEnum.Left,
+      [styles.right]: direction === DirectionEnum.Right
+    }, className)}
   >
-    <svg onClick={onClick} width="22" height="32" viewBox="0 0 22 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg onClick={onClick} {...props} width="22" height="32" viewBox="0 0 22 12" fill="none" xmlns="http://www.w3.org/2000/svg">
       <line x1="10.6464" y1="11.6237" x2="21.6237" y2="0.646447" stroke="white"/>
       <line x1="0.353553" y1="0.646447" x2="11.3308" y2="11.6237" stroke="white"/>
     </svg>
