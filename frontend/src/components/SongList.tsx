625abdb20f8e4cd308267cf0ae8544df527a4498
spark156 <lucabergman@hotmail.nl>
2021-12-28 11:37:41 +0100

    added adde song counter

--- frontend/src/components/SongList.tsx
+++ frontend/src/components/SongList.tsx
@@ -3,6 +3,7 @@ import { Item } from '../types/getPlaylistIDTracks'
 import { Song } from './Song'
 import { TitleDiv } from './TitleDiv'
 import { Container, ContainerDirectionEnum } from './Container'
+import { TextDiv } from './TextDiv'
 
 interface SavedSongsProps extends React.HTMLAttributes<HTMLDivElement> {
   items: Item[],
@@ -14,6 +15,7 @@ export function SongList ({ items, title }: SavedSongsProps) {
 
   return <Container disablePadding>
     <TitleDiv fontSize='20px'>{title}</TitleDiv>
+    <TextDiv>You added {items.length} song{items.length > 1 ? 's' : null} to this playlist</TextDiv>
     <Container style={{ paddingTop: 0 }} direction={ContainerDirectionEnum.Right}>
       {savedSongs.map(savedSong => <Song
       key={savedSong.track.id}
