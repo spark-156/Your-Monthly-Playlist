03053880d6e630278d42c234bb5b9480cd456ff6
spark156 <lucabergman@hotmail.nl>
2022-01-02 16:12:39 +0100

    fix permissions

--- backend/app/config.py
+++ backend/app/config.py
@@ -4,7 +4,7 @@ import os
 client_id = os.environ['CLIENT_ID']
 client_secret = os.environ['CLIENT_SECRET']
 redirect_uri = os.environ['REDIRECT_URI']
-access_scope = 'user-library-read'
+access_scope = 'user-library-read playlist-read-private'
 
 origins = [
   'http://localhost',
