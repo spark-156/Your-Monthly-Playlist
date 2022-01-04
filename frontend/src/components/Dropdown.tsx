6487baa4fe00e6bf5e5396ef2276d702c9d0a721
spark156 <lucabergman@hotmail.nl>
2022-01-04 14:20:23 +0100

    add bitTitle attribute to Dropdown

--- frontend/src/components/Dropdown.tsx
+++ frontend/src/components/Dropdown.tsx
@@ -7,15 +7,16 @@ import { Arrow, ArrowDirectionEnum } from './Arrow'
 
 interface DropdownProps extends React.HTMLAttributes<HTMLDivElement> {
   title: string,
+  bigTitle?: boolean,
   defaultOpen?: boolean
 }
 
-export function Dropdown ({ title, defaultOpen = false, children }: DropdownProps) {
+export function Dropdown ({ title, bigTitle = false, defaultOpen = false, children }: DropdownProps) {
   const [showChildren, setShowChildren] = useState<boolean>(defaultOpen)
 
   return <>
     <Container onClick={() => setShowChildren(prevState => !prevState)} disablePadding className={styles.dropdown} >
-      <TitleDiv className={styles.date} fontSize='24px'>{title}</TitleDiv>
+      <TitleDiv className={styles.date} fontSize={bigTitle ? '30px' : '22px'}>{title}</TitleDiv>
       {/* <Dots className={styles.dots} onClick={() => console.log('clicked')}/> */}
       <Arrow className={styles.arrowButton} direction={showChildren ? ArrowDirectionEnum.Up : ArrowDirectionEnum.Down} />
     </Container>
