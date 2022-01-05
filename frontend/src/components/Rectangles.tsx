fcfd75a5a27476454725a2c1c119ebe660d79a98
spark156 <lucabergman@hotmail.nl>
2022-01-05 22:57:59 +0100

    Rectangle icon

---
+++ frontend/src/components/Rectangles.tsx
@@ -0,0 +1,25 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import classNames from 'classnames'
+
+import styles from '../styles/Rectangle.module.css'
+
+interface RectanglesProps extends React.HTMLAttributes<SVGSVGElement> {}
+
+export function Rectangles ({ onClick, ...props }: RectanglesProps) {
+  return <svg
+    onClick={onClick}
+    style={{ cursor: 'pointer' }}
+    width="100%"
+    height="100%"
+    viewBox="0 0 50 50"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+
+      <rect y="40" width="50" height="5" rx="2.5" fill="white"/>
+      <rect y="20" width="50" height="5" rx="2.5" fill="white"/>
+      <rect width="50" height="5" rx="2.5" fill="white"/>
+    </svg>
+}
