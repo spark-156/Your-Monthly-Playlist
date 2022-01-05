c48f767703799a92fd75eaf8160cb92a205df31f
spark156 <lucabergman@hotmail.nl>
2022-01-05 21:30:57 +0100

    Create sliding animated modal

---
+++ frontend/src/components/Modal.tsx
@@ -0,0 +1,46 @@
+import React, { useEffect } from 'react'
+
+import ReactDOM from 'react-dom'
+
+import '../styles/Modal.css'
+import { Container } from './Container'
+import { CSSTransition } from 'react-transition-group'
+
+interface ModalProps extends React.HTMLAttributes<HTMLDivElement> {
+  show: boolean,
+  onClose: () => void,
+}
+
+export function Modal ({ show, onClose, children }: ModalProps) {
+  const root = document.getElementById('root')
+
+  if (root === null) return null
+
+  const closeOnEscapeDown = (e: KeyboardEvent) => {
+    if (e.key === 'Escape') onClose()
+  }
+
+  useEffect(() => {
+    document.body.addEventListener('keydown', closeOnEscapeDown)
+    return function cleanUp () {
+      document.body.removeEventListener('keydown', closeOnEscapeDown)
+    }
+  }, [])
+
+  return ReactDOM.createPortal(<CSSTransition
+    in={show}
+    unmountOnExit
+    timeout={{ enter: 300, exit: 300 }}
+  >
+    <div onClick={onClose} className="modal">
+      <Container onClick={e => e.stopPropagation()} className="modalContent">
+        <div className="modalHeader" >
+          <div onClick={onClose}>close me</div>
+        </div>
+        <div>
+          {children}
+        </div>
+      </Container>
+    </div>
+  </CSSTransition>, root)
+}
