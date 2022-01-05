a90498b998a1d315d4cf1bac4dc7c33debd337b3
spark156 <lucabergman@hotmail.nl>
2022-01-05 22:58:17 +0100

    implement rectangle icon

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -3,6 +3,7 @@ import { getCookie } from '../lib/getCookie'
 
 import styles from '../styles/Navbar.module.css'
 import { Modal } from './Modal'
+import { Rectangles } from './Rectangles'
 import { TextDiv } from './TextDiv'
 import { TitleDiv } from './TitleDiv'
 
@@ -13,7 +14,7 @@ export function Navbar () {
     <TitleDiv fontSize='30px' className={styles.title}>Your Monthly Playlist</TitleDiv>
     {getCookie('has_refresh_token')
       ? <>
-        <div onClick={() => setShowModal(true)}>modal</div>
+        <Rectangles className={styles.icon} onClick={() => setShowModal(true)} />
         <Modal show={showModal} onClose={() => setShowModal(false)}>
           <TextDiv clickable fontSize='24px' onClick={() => { window.location.href = '/api/v1/logout' }}>Logout</TextDiv>
         </Modal>
