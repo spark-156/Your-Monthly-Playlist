aabbf03c1c3f287d1fa736914e737239d8004c22
spark156 <lucabergman@hotmail.nl>
2022-01-05 12:10:30 +0100

    implement navbar with icon

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { getCookie } from '../lib/getCookie'
+
+import styles from '../styles/Navbar.module.css'
+import { TitleDiv } from './TitleDiv'
+
+export function Navbar () {
+  return <div className={styles.navbar}>
+    <TitleDiv fontSize='30px' className={styles.title}>Your Monthly Playlist</TitleDiv>
+    {getCookie('has_refresh_token') ? <TitleDiv fontSize='30px' className={styles.icon}>Icon</TitleDiv> : null}
+  </div>
+}
