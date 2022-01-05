45f340f32307d58c90f7179ee8cdc5b2e68c12fa
spark156 <lucabergman@hotmail.nl>
2022-01-05 21:31:23 +0100

    implement modal for logout call

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { getCookie } from '../lib/getCookie'
 
 import styles from '../styles/Navbar.module.css'
+import { Modal } from './Modal'
+import { TextDiv } from './TextDiv'
 import { TitleDiv } from './TitleDiv'
 
 export function Navbar () {
+  const [showModal, setShowModal] = useState<boolean>(false)
+
   return <div className={styles.navbar}>
     <TitleDiv fontSize='30px' className={styles.title}>Your Monthly Playlist</TitleDiv>
-    {getCookie('has_refresh_token') ? <TitleDiv fontSize='30px' onClick={() => { window.location.href = '/api/v1/logout' }} className={styles.icon}>Logout</TitleDiv> : null}
+    {getCookie('has_refresh_token')
+      ? <>
+        <div onClick={() => setShowModal(true)}>modal</div>
+        <Modal show={showModal} onClose={() => setShowModal(false)}>
+          <TextDiv onClick={() => { window.location.href = '/api/v1/logout' }}>Logout</TextDiv>
+        </Modal>
+      </>
+      : null}
   </div>
 }
