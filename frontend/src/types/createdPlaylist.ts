e59db991ecb66c66c896cd1da17410eaab768fae
spark156 <lucabergman@hotmail.nl>
2022-01-06 20:13:34 +0100

    add types

---
+++ frontend/src/types/createdPlaylist.ts
@@ -0,0 +1,46 @@
+/* eslint-disable camelcase */
+export interface CreatedPlaylist {
+  collaborative: boolean;
+  description: null;
+  external_urls: ExternalUrls;
+  followers: Followers;
+  href: string;
+  id: string;
+  images: any[];
+  name: string;
+  owner: Owner;
+  primary_color: null;
+  public: boolean;
+  snapshot_id: string;
+  tracks: Tracks;
+  type: string;
+  uri: string;
+}
+
+export interface ExternalUrls {
+  spotify: string;
+}
+
+export interface Followers {
+  href: null;
+  total: number;
+}
+
+export interface Owner {
+  display_name: string;
+  external_urls: ExternalUrls;
+  href: string;
+  id: string;
+  type: string;
+  uri: string;
+}
+
+export interface Tracks {
+  href: string;
+  items: any[];
+  limit: number;
+  next: null;
+  offset: number;
+  previous: null;
+  total: number;
+}
