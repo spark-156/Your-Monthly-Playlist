5f5492039e4e8d58242479f2984f3b5e00456904
spark156 <lucabergman@hotmail.nl>
2022-01-06 13:07:54 +0100

    add elipsis that logs all songs of the month to dropdown

--- frontend/src/components/Dropdown.tsx
+++ frontend/src/components/Dropdown.tsx
@@ -1,23 +1,27 @@
 import React, { useState } from 'react'
 import { Container } from './Container'
 import { TitleDiv } from './TitleDiv'
-import { DownOutlined, UpOutlined } from '@ant-design/icons'
+import { DownOutlined, UpOutlined, EllipsisOutlined } from '@ant-design/icons'
 
 import styles from '../styles/Dropdown.module.css'
+import { Item } from '../types/getPlaylistIDTracks'
 
 interface DropdownProps extends React.HTMLAttributes<HTMLDivElement> {
   title: string,
   bigTitle?: boolean,
-  defaultOpen?: boolean
+  defaultOpen?: boolean,
+  monthPlaylists?: {
+    [playlist: string]: Item[]
+  }
 }
 
-export function Dropdown ({ title, bigTitle = false, defaultOpen = false, children }: DropdownProps) {
+export function Dropdown ({ monthPlaylists, title, bigTitle = false, defaultOpen = false, children }: DropdownProps) {
   const [showChildren, setShowChildren] = useState<boolean>(defaultOpen)
 
   return <>
     <Container onClick={() => setShowChildren(prevState => !prevState)} disablePadding className={styles.dropdown} >
       <TitleDiv className={styles.date} fontSize={bigTitle ? '30px' : '22px'}>{title}</TitleDiv>
-      {/* <Dots className={styles.dots} onClick={() => console.log('clicked')}/> */}
+      {monthPlaylists ? <EllipsisOutlined style={{ fontSize: '25px' }} className={styles.dots} onClick={(e) => { e.stopPropagation(); console.log(monthPlaylists) }} /> : null}
       {showChildren ? <UpOutlined style={{ fontSize: '25px' }} className={styles.arrowButton} /> : <DownOutlined style={{ fontSize: '25px' }} className={styles.arrowButton} />}
     </Container>
     {showChildren ? children : null }
