067cb976289ccf7ac1aa1645dce76b7fad8b8449
spark156 <lucabergman@hotmail.nl>
2022-01-07 02:14:01 +0100

    refactor topGenres

--- frontend/src/components/Month.tsx
+++ frontend/src/components/Month.tsx
@@ -1,6 +1,9 @@
+/* eslint-disable no-unused-vars */
+import _ from 'lodash'
 import { DateTime } from 'luxon'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { createPlaylist } from '../lib/createPlaylist'
+import { getArtists } from '../lib/getArtists'
 import { Playlist } from '../types/getMePlaylists'
 import { Item } from '../types/getPlaylistIDTracks'
 import { Container } from './Container'
@@ -22,14 +25,42 @@ interface MonthProps extends React.HTMLAttributes<HTMLDivElement> {
 export function Month ({ playlists, year, month, monthPlaylists }: MonthProps) {
   const now = DateTime.now().setLocale('en-GB')
   const playlistTitle = `${month} ${year}`
-  // eslint-disable-next-line no-unused-vars
   const [monthPlaylist, setMonthPlaylist] = useState(playlists.filter(playlist => playlist.name === playlistTitle))
   const [showModal, setShowModal] = useState<boolean>(false)
+  const [topGenres, setTopGenres] = useState<string[]>([])
+  const [loadingTopGenres, setLoadingTopGenres] = useState<boolean>(true)
+
+  useEffect(() => {
+    async function get () {
+      const artistIds: string[] = _
+        .chain(monthPlaylists)
+        .values()
+        .flatten()
+        .map(song => song.track.artists)
+        .flatten()
+        .map(artist => artist.id)
+        .value()
+      const uniqueArtistIds: string[] = _.uniq(artistIds)
+      const uniqueArtists = await getArtists([...uniqueArtistIds])
+      const genresList: string[] = []
+      uniqueArtistIds.forEach(id => {
+        uniqueArtists.filter(artist => artist.id === id).forEach(artist => artist.genres.forEach(genre => genresList.push(genre)))
+      })
+      const genres: {[key: string]: number} = {}
+      for (const genre of genresList) {
+        genres[genre] = genres[genre] ? genres[genre] + 1 : 1
+      }
+      const items: [string, number][] = Object.keys(genres).map(key => [key, genres[key]])
+      setTopGenres(items.sort((first, second) => second[1] - first[1]).slice(0, 5).map(item => item[0]))
+      setLoadingTopGenres(false)
+    }
+    get()
+  }, [])
 
   return <>
     <Dropdown defaultOpen={year === now.year.toString() && month === now.monthLong} modalTitle={playlistTitle} title={month} setShowModal={setShowModal} >
       <Container disablePaddingTopAndBottom>
-        <TopGenres month={monthPlaylists} />
+        <TopGenres topGenres={topGenres} loading={loadingTopGenres} />
         {Object.keys(monthPlaylists).map(playlistName => <SongList key={`${year}${month}${playlistName}`} title={playlistName} items={monthPlaylists[playlistName]} />) }
       </Container>
     </Dropdown>
--- frontend/src/components/TopGenres.tsx
+++ frontend/src/components/TopGenres.tsx
@@ -1,48 +1,15 @@
-import React, { useEffect, useState } from 'react'
-import { getArtists } from '../lib/getArtists'
-import { Item } from '../types/getPlaylistIDTracks'
+import React from 'react'
 import { Container } from './Container'
 import { Loading } from './Loading'
 import { TextDiv } from './TextDiv'
 import { TitleDiv } from './TitleDiv'
-import { flatten } from 'lodash'
+
 interface TopGenresProps extends React.HTMLAttributes<HTMLDivElement> {
-  month: {[playlist: string]: Item[]}
+  topGenres: string[],
+  loading: boolean
 }
 
-export function TopGenres ({ month }: TopGenresProps) {
-  const [loading, setLoading] = useState<boolean>(true)
-  const [topGenres, setTopGenres] = useState<string[]>([])
-
-  useEffect(() => {
-    async function get () {
-      const songs = flatten(Object.keys(month).map(key => month[key]))
-      const artistsIds: string[] = []
-      songs.forEach(item => item.track.artists.forEach(artist => artistsIds.push(artist.id)))
-      const uniqueArtistIds = artistsIds.filter(function (item, pos) { return artistsIds.indexOf(item) === pos })
-
-      const uniqueArtists = await getArtists([...uniqueArtistIds])
-
-      const genresList: string[] = []
-
-      uniqueArtistIds.forEach(id => {
-        uniqueArtists.filter(artist => artist.id === id).forEach(artist => artist.genres.forEach(genre => genresList.push(genre)))
-      })
-
-      const genres: {[key: string]: number} = {}
-
-      for (const genre of genresList) {
-        genres[genre] = genres[genre] ? genres[genre] + 1 : 1
-      }
-
-      const items: [string, number][] = Object.keys(genres).map(key => [key, genres[key]])
-      setTopGenres(items.sort((first, second) => second[1] - first[1]).slice(0, 5).map(item => item[0]))
-
-      setLoading(false)
-    }
-    get()
-  }, [])
-
+export function TopGenres ({ topGenres, loading }: TopGenresProps) {
   if (loading) return <Loading />
 
   return <Container disablePadding halfGap>
