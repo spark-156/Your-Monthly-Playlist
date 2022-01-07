c677d821421ce4cde5e66da3663fc10c4d1169ed
spark156 <lucabergman@hotmail.nl>
2022-01-07 01:54:58 +0100

    refactor modal away from dropdown into month

--- frontend/src/components/Month.tsx
+++ frontend/src/components/Month.tsx
@@ -1,11 +1,13 @@
-/* eslint-disable no-unused-vars */
 import { DateTime } from 'luxon'
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
+import { createPlaylist } from '../lib/createPlaylist'
 import { Playlist } from '../types/getMePlaylists'
 import { Item } from '../types/getPlaylistIDTracks'
 import { Container } from './Container'
 import { Dropdown } from './Dropdown'
+import { Modal } from './Modal'
 import { SongList } from './SongList'
+import { TextDiv } from './TextDiv'
 import { TopGenres } from './TopGenres'
 
 interface MonthProps extends React.HTMLAttributes<HTMLDivElement> {
@@ -17,16 +19,22 @@ interface MonthProps extends React.HTMLAttributes<HTMLDivElement> {
   playlists: Playlist[],
 }
 
-export function Month ({ playlists, year, month, monthPlaylists, ...props }: MonthProps) {
+export function Month ({ playlists, year, month, monthPlaylists }: MonthProps) {
   const now = DateTime.now().setLocale('en-GB')
-  const [monthPlaylist, setMonthPlaylist] = useState(playlists.filter(playlist => playlist.name === `${month} ${year}`))
+  const playlistTitle = `${month} ${year}`
+  // eslint-disable-next-line no-unused-vars
+  const [monthPlaylist, setMonthPlaylist] = useState(playlists.filter(playlist => playlist.name === playlistTitle))
+  const [showModal, setShowModal] = useState<boolean>(false)
 
-  console.log(monthPlaylist)
-
-  return <Dropdown monthPlaylists={monthPlaylists} defaultOpen={year === now.year.toString() && month === now.monthLong} modalTitle={`${month} ${year}`} title={month} >
-    <Container disablePaddingTopAndBottom>
-      <TopGenres month={monthPlaylists} />
-      {Object.keys(monthPlaylists).map(playlistName => <SongList key={`${year}${month}${playlistName}`} title={playlistName} items={monthPlaylists[playlistName]} />) }
-    </Container>
-  </Dropdown>
+  return <>
+    <Dropdown defaultOpen={year === now.year.toString() && month === now.monthLong} modalTitle={playlistTitle} title={month} setShowModal={setShowModal} >
+      <Container disablePaddingTopAndBottom>
+        <TopGenres month={monthPlaylists} />
+        {Object.keys(monthPlaylists).map(playlistName => <SongList key={`${year}${month}${playlistName}`} title={playlistName} items={monthPlaylists[playlistName]} />) }
+      </Container>
+    </Dropdown>
+    <Modal title={''} show={showModal} onClose={() => setShowModal(false)}>
+      <TextDiv fontSize='24px' clickable onClick={() => createPlaylist(playlistTitle, monthPlaylists)}>Create playlist</TextDiv>
+    </Modal>
+  </>
 }
