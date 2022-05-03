f285aa14fa9df0acebb2e22ee0e168f83e18c170
spark-156 <lucabergman@hotmail.nl>
2022-05-03 11:19:41 +0200

    refactor playlists fetching and refreshing

--- app/pages/playlists.vue
+++ app/pages/playlists.vue
@@ -1,58 +1,68 @@
 <template>
-  <v-row>
-    <v-col
-      cols="12"
-    >
-      <v-card>
-        <v-card-title>Playlists</v-card-title>
-        <v-card-subtitle>{{ numberOfPlaylistsString }} found</v-card-subtitle>
+  <div>
+    <v-row>
+      <v-col
+        cols="12"
+      >
+        <v-card>
+          <v-card-title>Playlists</v-card-title>
+          <v-card-subtitle>{{ numberOfPlaylistsString }} found</v-card-subtitle>
 
-        <v-card-text>
-          Please select all the playlists you would like to include in the calculation of your monthly playlists.
-        </v-card-text>
+          <v-card-text>
+            Please select all the playlists you would like to include in the calculation of your monthly playlists.
+          </v-card-text>
 
-        <v-card-actions>
-          <v-btn
-            :loading="!hasLoaded"
-            @click="refreshPlaylists"
-            color="green"
-          >
-            REFRESH
-          </v-btn>
-          <v-btn @click="log">
-            DEBUG LOG
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
+          <v-card-actions>
+            <v-btn
+              :loading="!hasLoaded"
+              color="green"
+              @click="refreshPlaylists"
+            >
+              REFRESH
+            </v-btn>
+            <v-btn @click="log">
+              DEBUG LOG
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
 
-    <v-col
-      v-if="$fetchState.pending"
-      cols="12"
-    >
-      <v-progress-circular
-        indeterminate
-        color="primary"
-      />
-    </v-col>
+      <v-col
+        v-if="$fetchState.pending"
+        cols="12"
+      >
+        <v-progress-circular
+          indeterminate
+          color="primary"
+        />
+      </v-col>
 
-    <v-col
-      v-for="(item, i) in unselectedPlaylists"
-      :key="i"
-      cols="6"
-      sm="3"
-      lg="2"
-      xl="1"
-    >
-      <playlist-card
-        :title="item.playlist.name"
-        :number-of-songs="item.playlist.tracks.total"
-        :image="item.playlist.images[0]"
-        :selected="item.selected"
-        @toggleSelected="toggle(item)"
-      />
-    </v-col>
-  </v-row>
+      <v-col
+        v-for="(item, i) in playlists"
+        :key="i"
+        cols="6"
+        sm="3"
+        lg="2"
+        xl="1"
+      >
+        <playlist-card
+          :title="item.playlist.name"
+          :number-of-songs="item.playlist.tracks.total"
+          :image="item.playlist.images[0]"
+          :selected="item.selected"
+          @toggleSelected="toggle(item)"
+        />
+      </v-col>
+    </v-row>
+    <v-footer fixed>
+      <v-col
+        class="text-center"
+        cols="12"
+      >
+        {{ selectectedPlaylistsCount }} playlists selected
+      </v-col>
+    </v-footer>
+  </div>
 </template>
 
 <script>
@@ -67,7 +77,7 @@ export default {
   },
   async fetch () {
     if (!this.hasLoaded) {
-      await this.refreshPlaylists()
+      await this.$store.commit('playlists/getPlaylists')
     }
   },
   fetchOnServer: false,
@@ -81,34 +91,20 @@ export default {
       if (this.numberOfSongs === 1) { word = 'playlists' }
       return `${this.$store.state.playlists.amount} ${word}`
     },
-    unselectedPlaylists () {
-      return this.$store.state.playlists.list.filter(item => !item.selectected)
+    playlists () {
+      return this.$store.state.playlists.list
     },
-    selectectedPlaylists () {
-      return this.$store.state.playlists.list.filter(item => item.selectected)
+    selectectedPlaylistsCount () {
+      return this.$store.state.playlists.list.filter(item => item.selected).length
     }
   },
   methods: {
-    async getPlaylists () {
-      let res = { next: '/me/playlists?limit=50' }
-      do {
-        res = await this.$axios.$get(res.next)
-        for (const item of res.items) {
-          this.$store.commit('playlists/add', item)
-        }
-      } while (res.next)
-      this.$store.commit('playlists/setAmount', res.total)
-    },
-    async refreshPlaylists () {
-      this.$store.commit('playlists/setHasLoadedFalse')
-      this.$store.commit('playlists/empty')
-      await this.getPlaylists()
-      this.$store.commit('playlists/setHasLoadedTrue')
-    },
     ...mapMutations({
-      toggle: 'playlists/toggle'
+      toggle: 'playlists/toggle',
+      refreshPlaylists: 'playlists/refreshPlaylists'
     }),
     log () {
+      console.log(this.$store.state.playlists.list.filter(item => item.selected).length)
       console.log(this.$store.state.playlists.list)
       console.log(this.$store.state.playlists.list.filter(item => item.selected))
     }
--- app/store/playlists.js
+++ app/store/playlists.js
@@ -5,6 +5,24 @@ export const state = () => ({
 })
 
 export const mutations = {
+  async getPlaylists (_state) {
+    this.commit('playlists/setHasLoadedFalse')
+    let res = { next: '/me/playlists?limit=50' }
+    do {
+      res = await this.$axios.$get(res.next)
+      for (const item of res.items) {
+        this.commit('playlists/add', item)
+      }
+    } while (res.next)
+    this.commit('playlists/setAmount', res.total)
+    this.commit('playlists/setHasLoadedTrue')
+  },
+  async refreshPlaylists (_state) {
+    this.commit('playlists/setHasLoadedFalse')
+    this.commit('playlists/empty')
+    await this.commit('playlists/getPlaylists')
+    this.commit('playlists/setHasLoadedTrue')
+  },
   add (state, playlist) {
     state.list.push({ playlist, selected: false })
   },
